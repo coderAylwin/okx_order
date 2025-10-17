faec81ccda6be205716fee6f7c54ccb0ac670c5c
Aylwin <coderaylwin@gmail.com>
2025-10-18 00:05:14 +0800

    fix database_config.py

--- okx_trend_sar_single_period_boll/database_config.py
+++ okx_trend_sar_single_period_boll/database_config.py
@@ -12,10 +12,12 @@ DATABASE_CONFIG = {
 
 # 自己本地数据库配置
 LOCAL_DATABASE_CONFIG = {
-    'host': '127.0.0.1',     # 本地MySQL
+    'host': 'rm-t4nea067q32i31k9aro.mysql.singapore.rds.aliyuncs.com',     # 本地MySQL
     'port': 3306,
-    'user': 'root',
-    'password': '',           # 请修改为您的MySQL密码
+    'user': 'payment_pro',
+    'password': 'nS4kO7tG1jH7cI6oR4b',           # 请修改为您的MySQL密码
     'database': 'quantify',    # 数据库名称
-    'charset': 'utf8mb4'
+    'auth_plugin': '',
+    'pool_name': 'okx_pool',  # 添加简短的连接池名称
+    'pool_size': 10,  # 设置连接池大小
 }
