23ff1613cd04d76e4901c5e03fcbed450470742d
Aylwin <coderaylwin@gmail.com>
2025-11-08 02:48:33 +0800

    fix database_config

--- okx_trend_volumatic_dynamic_average/database_config.py
+++ okx_trend_volumatic_dynamic_average/database_config.py
@@ -11,19 +11,19 @@ DATABASE_CONFIG = {
 }
 
 # 自己本地数据库配置
-LOCAL_DATABASE_CONFIG = {
-    'host': '127.0.0.1',     # 本地MySQL
-    'port': 3306,
-    'user': 'root',
-    'password': '',           # 请修改为您的MySQL密码
-    'database': 'quantify'    # 数据库名称
-}
-
 # LOCAL_DATABASE_CONFIG = {
-#     'host': 'rm-t4nea067q32i31k9aro.mysql.singapore.rds.aliyuncs.com',     # 本地MySQL
+#     'host': '127.0.0.1',     # 本地MySQL
 #     'port': 3306,
-#     'user': 'payment_pro',
-#     'password': 'nS4kO7tG1jH7cI6oR4b',           # 请修改为您的MySQL密码
-#     'database': 'quantify',    # 数据库名称
-#     'charset': 'utf8mb4',
+#     'user': 'root',
+#     'password': '',           # 请修改为您的MySQL密码
+#     'database': 'quantify'    # 数据库名称
 # }
+
+LOCAL_DATABASE_CONFIG = {
+    'host': 'rm-t4nea067q32i31k9aro.mysql.singapore.rds.aliyuncs.com',     # 本地MySQL
+    'port': 3306,
+    'user': 'payment_pro',
+    'password': 'nS4kO7tG1jH7cI6oR4b',           # 请修改为您的MySQL密码
+    'database': 'quantify',    # 数据库名称
+    'charset': 'utf8mb4',
+}
